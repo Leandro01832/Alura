d45098678df49d15ff6221a2d2958c2967a9eafb
Leandro Luis da Silva <leandroleanleo91@outlook.com>
2020-12-30 12:05:00 -0300

    Curso parte 3

---
+++ aulas-alura/curso parte 3/ASPNETCore2-Parte3-master/antes/Item04/CasaDoCodigo/Repositories/HttpHelper.cs
@@ -0,0 +1,48 @@
+﻿using CasaDoCodigo.Areas.Identity.Data;
+using CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace CasaDoCodigo
+{
+    //TAREFA 05: INJETAR UserManager PARA OBTER clienteId
+    public class HttpHelper : IHttpHelper
+    {
+        private readonly IHttpContextAccessor contextAccessor;
+        private readonly UserManager<AppIdentityUser> userManager;
+
+        public IConfiguration Configuration { get; }
+
+        public HttpHelper(IHttpContextAccessor contextAccessor, IConfiguration configuration,
+            UserManager<AppIdentityUser> userManager)
+        {
+            this.contextAccessor = contextAccessor;
+            Configuration = configuration;
+            this.userManager = userManager;
+        }
+
+        public int? GetPedidoId()
+        {
+            return contextAccessor.HttpContext.Session.GetInt32($"pedidoId{GetClienteId()}");
+        }
+
+        public void SetPedidoId(int pedidoId)
+        {
+            contextAccessor.HttpContext.Session.SetInt32($"pedidoId{GetClienteId()}", pedidoId);
+        }
+
+        private string GetClienteId()
+        {
+            var claimsPrincipal = contextAccessor.HttpContext.User;
+            var ClienteId = userManager.GetUserId(claimsPrincipal);
+            return ClienteId;
+        }
+
+        public void ResetPedidoId()
+        {
+            contextAccessor.HttpContext.Session.Remove($"pedidoId{GetClienteId()}");
+        }
+    }
+}
