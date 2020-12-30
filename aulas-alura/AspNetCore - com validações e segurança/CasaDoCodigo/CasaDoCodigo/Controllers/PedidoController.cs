caf2b9cc8e539f63a094ee36fd98b2a117f410d7
Leandro Luis da Silva <leandroleanleo91@outlook.com>
2020-12-30 10:24:46 -0300

    AspNetCore - Segurança e validações

---
+++ aulas-alura/AspNetCore - com validações e segurança/CasaDoCodigo/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,57 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IProdutoRepository repostory;
+        private readonly IPedidoRepository Pedidorepostory;
+        private readonly IItemPedidoRepository ItemPedidorepostory;
+
+        public PedidoController(IProdutoRepository repostory, IPedidoRepository Pedidorepostory, IItemPedidoRepository ItemPedidorepostory)
+        {
+            this.repostory = repostory;
+            this.Pedidorepostory = Pedidorepostory;
+            this.ItemPedidorepostory = ItemPedidorepostory;
+        }
+
+        public IActionResult Carrossel()
+        {
+            return View(repostory.GetProdutos());
+        }
+
+        public IActionResult Carrinho(string codigo)
+        {
+            if (!string.IsNullOrEmpty(codigo))
+            {
+                Pedidorepostory.AddItem(codigo);
+            }
+
+            Pedido pedido = Pedidorepostory.GetPedido();
+            return View(pedido.Itens);
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo()
+        {
+            Pedido pedido = Pedidorepostory.GetPedido();
+            return View(pedido);
+        }
+
+        [HttpPost]
+        public void UpdateQuantidade([FromBody]ItemPedido itemPedido)
+        {
+            ItemPedidorepostory.UpdateQuantidade(itemPedido);
+        }
+    }
+}
---
+++ aulas-alura/AspNetCore - com validações e segurança/CasaDoCodigo/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -0,0 +1,33 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+
+   public interface IItemPedidoRepository
+    {
+        void UpdateQuantidade(ItemPedido itemPedido);
+    }
+
+    public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
+    {
+        public ItemPedidoRepository(AplicationContext contexto) : base(contexto)
+        {
+        }
+
+        public void UpdateQuantidade(ItemPedido itemPedido)
+        {
+            ItemPedido itemPedidoDB = dbSet.Where(i => i.Id == itemPedido.Id).SingleOrDefault();
+
+            if (itemPedidoDB != null)
+            {
+
+                itemPedidoDB.AtualizaQuantidade(itemPedido.Quantidade);
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
